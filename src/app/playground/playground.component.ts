1f707c720dd17fef3fcb4567b7728d34461baf44
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-28 16:33:47 +0530

    finished

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
+  @Input() title: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
