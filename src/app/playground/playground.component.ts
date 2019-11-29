a2adee7d6c635d142aae0d409fe6863be991e1c6
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-29 15:00:13 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,8 +1,21 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent {}
+export class PlaygroundComponent {
+  @ViewChild('para', { static: true }) set pEl(value: ElementRef) {
+    console.log('Element', value);
+    value.nativeElement.className = 'font-weight-bold';
+  }
+}
